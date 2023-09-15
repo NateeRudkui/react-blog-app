b19c8f7d5582e2949f9f5489a4959e4beb329ca2
Natee Rudkui <nateerudkui@outlook.com>
2023-09-15 11:36:47 +0700

    ✅ : add Navigate

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Blogs } from "./components/Blogs";
 import { About } from "./components/about";
 import { Home } from "./components/home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { Notfound } from "./components/Notfound";
 
@@ -16,6 +16,7 @@ function App() {
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<Notfound />} />
+          <Route path="/home" element={<Navigate to={"/"} />} />
         </Routes>
       </BrowserRouter>
     </div>
