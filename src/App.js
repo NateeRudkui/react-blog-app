70e871ff3446ff776ccc75f79b7f5cb6796d058d
Natee Rudkui <nateerudkui@outlook.com>
2023-09-15 08:10:20 +0700

    ✅[Add] : Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Blogs } from "./components/Blogs";
+import { About } from "./components/About";
 import { Home } from "./components/home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
