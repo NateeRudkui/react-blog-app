a778f0eb86fd9ee27ef571d4746643a82c9c2105
Natee Rudkui <nateerudkui@outlook.com>
2023-09-18 08:01:24 +0700

    💄 : add style blog img

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import blogs from "../data/blogs";
 import { useEffect, useState } from "react";
+import "./Detail.css";
 
 export default function Detail() {
   const { id } = useParams();
@@ -20,7 +21,7 @@ export default function Detail() {
   return (
     <div className="container">
       <h2 className="title">บทความ : {title}</h2>
-      <img src={image_url} alt="title" />
+      <img src={image_url} alt="title" className="blog_img" />
       <div className="blog-detail">
         <strong>{author}</strong>
         <p>{content}</p>
